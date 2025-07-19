6995b548e379bde33369d8c85e421cb1aae839dd
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2025-07-19 21:35:20 +0200

    Remaked Sequence so that acts like a base class

--- PySVBench/generate.py
+++ PySVBench/generate.py
@@ -1,4 +1,7 @@
 from math import ceil
+from dataclasses import dataclass as _dataclass
+from typing import Generator as _Generator
+from warnings import warn as _warn
 
 """
 Generate sequences for testbenches easily.
@@ -7,79 +10,42 @@ Generate sequences for testbenches easily.
 class Sequence:
 
     """
-    Represents a sequence of test vectors
+    Base class of all sequence types
     """
 
-    def __init__(self, name_sequence : str = "test_sequence", bits : int = 8, sequence : list[str] = [], default_value : str = "0", sequence_lenght : int = -1):
+    def __init__(self, name: str, bits: int, sequence: list[str], generator: _Generator[str], sequence_lenght: int = -1):
 
-        """
-        Creates a sequence:
-            - name_sequence: the name of the sequence
-            - bits: the width in bits of the sequence
-            - sequence: the sequence indeed, by default it is empty.
-            - default_value: the default value that the sequence takes (if it has to fill values)
-            - sequence_lenght: the lenght of the sequence, by default is undefined (the lenght of the list in fact)
-        """
 
-        self.name_sequence = name_sequence
+        self.name = name
         self.bits = bits
         self.sequence = sequence
-        self.default_value = default_value
+        self.generator = generator
         self.sequence_lenght = sequence_lenght
-    
-    def add_elements(self, elements : list[str]):
+
+    def generate(self, lenght_override: int = -1):
 
         """
-        Add an interable of elements to the sequence
+        Generates the sequence using the default sequence_lenght and generator. If lenght_override is positive, it will be used as lenght instead of sequence_lenght.
         """
 
-        self.sequence.extend(elements)
+        if lenght_override > 0:
 
-    def set_elements(self, elements : list[str]):
+            self.sequence = [next(self.generator) for _ in range(lenght_override)]
 
-        """
-        Sets the sequence to be equal to the iterable "elements"
-        """
+        else:
+
+            if self.sequence_lenght > 0:
+
+                self.sequence = [next(self.generator) for _ in range(self.sequence_lenght)]
+
+            else:
+
+                _warn("Sequence not generated because lenght would be infinite. Expected to be generated manually")
+
+    def add_elements(self, elements: list[str]):
+
+        self.sequence += elements
+
+    def set_elements(self, elements: list[str]):
 
         self.sequence = elements
-
-    def create_file(self):
-
-        """
-        Creates the test vector file of the sequence, in the specified codec.
-        """
-
-        print(str(self) + ":", "creating file")
-
-        with open(self.name_sequence + ".mem", "w") as file:
-
-            print(str(self) + ":", "File created")
-
-            if self.sequence_lenght == -1:
-
-                print(str(self) + ":", "adding full sequence")
-
-                for element in self.sequence:
-
-                    file.write(element.rjust(self.bits, "0") + "\n")
-            else:
-                print(str(self) + ":", "adding full sequence + default values")
-                
-                for x in range(self.sequence_lenght):
-                    
-                    if x < len(self.sequence):
-
-                        file.write(self.sequence[x].rjust(self.bits, "0") + "\n")
-
-                    else:
-                        
-                        file.write(self.default_value.rjust(self.bits, "0") + "\n")
-
-        print(str(self) + ":", "file created")
-
-
-    def __repr__(self):
-        return "{Sequence: name_sequence: {" + self.name_sequence + "}, bits: {" + str(self.bits) + "}, sequence: {" + str(self.sequence) + "}}"
-    
-    def __str__(self):
-        return "[Sequence: " + self.name_sequence + "]"
