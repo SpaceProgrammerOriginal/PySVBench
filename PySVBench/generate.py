0acf8b94b5b78c46188ba2f9eed21009a37da955
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2025-07-23 10:07:53 +0200

    Created Testbench class
    Also deleted Sequence and other things now moved to basetypes

--- PySVBench/generate.py
+++ PySVBench/generate.py
@@ -1,85 +1,13 @@
-from math import ceil
-from dataclasses import dataclass as _dataclass
-from typing import Generator as _Generator
-from warnings import warn as _warn
+from .basetypes import TestbenchSignal as _TestbenchSignal
+from .basetypes import TestbenchSequence as _TestbenchSequence
+from .basetypes import Clock as _Clock
+from .basetypes import OutputSignal as _OutputSignal
+from .basetypes import CodeBlock as _CodeBlock
+from .basetypes import BlockType as _BlockType
 
 """
 Generate sequences for testbenches easily.
-"""
-
-class Sequence:
-
-    """
-    Base class of all sequence types
-    """
-
-    def __init__(self, name: str, bits: int, sequence: list[str], generator: _Generator[str], sequence_lenght: int = -1):
-
-
-        self.name = name
-        self.bits = bits
-        self.sequence = sequence
-        self.generator = generator
-        self.sequence_lenght = sequence_lenght
-
-    def generate(self, lenght_override: int = -1):
-
-        """
-        Generates the sequence using the default sequence_lenght and generator. If lenght_override is positive, it will be used as lenght instead of sequence_lenght.
-        """
-
-        if lenght_override > 0:
-
-            self.sequence = [next(self.generator) for _ in range(lenght_override)]
-
-        else:
-
-            if self.sequence_lenght > 0:
-
-                self.sequence = [next(self.generator) for _ in range(self.sequence_lenght)]
-
-            else:
-
-                _warn("Sequence not generated because lenght would be infinite. Expected to be generated manually")
-
-    def add_elements(self, elements: list[str]):
-
-        self.sequence += elements
-
-    def set_elements(self, elements: list[str]):
-
-        self.sequence = elements
-
-class Clock(Sequence):
-
-    """
-    A sequence representing a clock.
-    """
-
-    def __init__(self, name, period: int):
-
-        def clock_generator():
-
-            while True:
-                yield "0"
-                yield "1"
-
-        self.period = period
-        gen = clock_generator()
-
-        super().__init__(name=name, bits=1, sequence=[], generator=gen, sequence_lenght=-1)
-
-    def generate(self, lenght_override = -1):
-
-        return super().generate(lenght_override)
-    
-class Testvector(Sequence):
-    
-    """
-    A sequence representing a vector of values.
-    """
-
-    pass #literally is simply a direct wrapper of Sequence.
+"""    
 
 class Testbench:
 
@@ -87,23 +15,29 @@ class Testbench:
     Generates a full testbench of systemverilog, consisting on a group of sequences.
     """
 
-    def __init__(self, testbench_name: str,  *sequences: Sequence, filepath: str = "", ):
+    def __init__(self, testbench_name: str, testbench_element: str, input_elements: list[_TestbenchSignal | _TestbenchSequence], output_elements: list[_OutputSignal], simulation_steps: int, filepath: str = ""):
 
         """
-        - testbench_name = the name of the testbench. It becomes the name of the module that will have the testbench in systemverilog
-        - *sequences = all the sequences you want for the testbench.
+        - testbench_name = the name of the testbench. It becomes the name of the module that will have the testbench in systemverilog.
+        - testbench_element = the name of the module to be testbenched.
+        - input_elements = all the sequences you want for the testbench.
+        - output_elements = all the output elements of the testbench.
+        - simulation_steps = the number of steps that the sequences will be generated to.
         - filepath = the result filepath of the testbench. By default is [testbench name].sv
         """
 
         self.testbench_name = testbench_name
+        self.testbench_element = testbench_element
+        self.input_elements = input_elements
+        self.output_elements = output_elements
+        self.simulation_steps = simulation_steps
         self.filepath = filepath
-        self.sequences = list(sequences)
 
         self.code = ""
 
-    def set_sequences(self, *sequences: Sequence):
+    def set_input_elements(self, input_elements: list[_TestbenchSignal | _TestbenchSequence]):
 
-        self.sequences = list(sequences)
+        self.input_elements = input_elements
 
     def update_testbench(self):
 
@@ -111,13 +45,98 @@ class Testbench:
         Creates (if not created) and updates the internal representation of the testbench
         """
 
-        self.code = "module " + self.testbench_name + "();\n"
+        codeblocks = self._update_testbench_phase1()
+        final_str = self._update_testbench_phase2(codeblocks)
+        self._update_testbench_phase3(final_str)
 
-        initial_code = "initial begin\n"
-        always_code = "always begin\n"
+    def _update_testbench_phase1(self):
 
-        for sequence in self.sequences:
-            pass
+        """
+        The first phase of updating the testbench.
 
+        Consists on creating codeblocks and propagating through all input_elements
+        """
 
-        self.code += "    var logic"
+        codeblocks = [_CodeBlock(_BlockType.EXTERN), _CodeBlock(_BlockType.INITIAL), _CodeBlock(_BlockType.ALWAYS)] #the three basic codeblocks
+
+        order_elements = []
+
+        #generates the code for all clocks (because clocks define extra codeblocks and must be put before for avoiding errors)
+        for input_element in self.input_elements:
+
+            if type(input_element) == _Clock:
+                
+                input_element.generate_code(codeblocks)
+
+        for input_element in self.input_elements:
+
+            order_elements.append(input_element.name)
+
+            if isinstance(input_element, _TestbenchSignal):
+                input_element.generate_code()
+            elif isinstance(input_element, _TestbenchSequence):
+                input_element.generate(self.simulation_steps)
+                input_element.create_file()
+                input_element.generate_code(codeblocks)
+            else:
+                raise TypeError("Input element is neither TestbenchSignal nor TestbenchSequence")
+            
+        for output_element in self.output_elements:
+
+            order_elements.append(output_element.name)
+            output_element.generate_code(codeblocks)
+
+        return codeblocks
+    
+    def _update_testbench_phase2(self, codeblocks: list[_CodeBlock]):
+
+        """
+        Second phase of updating testbench. Consists on generating the final string.
+        """
+
+        final_str = "module " + self.testbench_name + "();\n"
+
+        for codeblock in codeblocks:
+
+            if codeblock.blocktype == _BlockType.EXTERN:
+
+                for line in codeblock.codegen:
+                    final_str += "    " + line + "\n"
+
+            elif codeblock.blocktype == _BlockType.INITIAL:
+
+                #create the block and add it to the final string
+                final_str += "    " + "initial begin\n"
+                for line in codeblock.codegen:
+                    final_str += "    " * 2 + line + "\n"
+                final_str += "    " + "end\n"
+
+            elif codeblock.blocktype == _BlockType.ALWAYS:
+                
+                if "clock" in codeblock.metadata:
+
+                    final_str += "    " + "always @(posedge " + codeblock.metadata["clock"].name + ") begin\n"
+                    for line in codeblock.codegen:
+                        final_str += "    " * 2 + line + "\n"
+                    final_str += "    " + "end\n"
+
+                else:
+
+                    final_str += "    " + "always begin\n"
+                    for line in codeblock.codegen:
+                        final_str += "    " * 2 + line + "\n"
+                    final_str += "    " + "end\n"
+
+            else:
+                raise NotImplementedError("Currently no more codeblock types implemented than EXTERN, INITIAL, and ALWAYS!")
+            
+        return final_str
+    
+    def _update_testbench_phase3(self, final_str: str):
+
+        """
+        The final phase of updating the testbench. Consisting on writing the string to the filepath
+        """
+
+        with open(self.filepath, "w") as file:
+            file.write(final_str)
