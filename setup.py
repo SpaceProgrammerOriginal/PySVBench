e7ce68ad9bfd50ed5bdd3f5b6b67801c3b3a3a8a
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2025-07-29 22:46:14 +0200

    Updated to 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PySVBench",
-    version="0.5.3",
+    version="0.6",
     packages=find_packages(),
 
     long_description=open("README.md", "r").read(),
